83bae38ec39c6ca29997e69d3bb9a0886a2ce33a
shobhit singh <shobhitsingh29@gmail.com>
2020-11-28 21:58:23 +0530

    ghjk

--- src/components/Clubs/index.jsx
+++ src/components/Clubs/index.jsx
@@ -1,26 +1,31 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import SeatList from "../seatList";
 
 export default function Clubs(props) {
-    debugger;
-    const {label, totalSeats, seatInARow, maxSelection,userSelection}=props;
-debugger
-    const noOfRows = Math.floor(totalSeats / seatInARow);
+  debugger;
+  const { label, totalSeats, seatInARow, maxSelection, userSelection } = props;
+  const noOfRows = Math.floor(totalSeats / seatInARow);
 
-    const totalRows = Array.from(Array(noOfRows).keys());
-    const isSelectedArr = {
-        [label]: [[]]
-    };
-    return (
-        <div>
-            {label}
-            {totalRows?.map((value => {
-                return <SeatList key={value}
-                                 isSelectedArr={isSelectedArr[label]}
-                                 userSelection={userSelection}
-                                 maxSelection={maxSelection}
-                                 ns={seatInARow} rowNo={value}/>
-            }))}
-        </div>
-    );
+  const totalRows = Array.from(Array(noOfRows).keys());
+  const isSelectedArr = {
+    [label]: [[]]
+  };
+
+  return (
+    <div>
+      {label}
+      {totalRows?.map((value) => {
+        return (
+          <SeatList
+            key={value}
+            isSelectedArr={isSelectedArr[label]}
+            userSelection={userSelection}
+            maxSelection={maxSelection}
+            ns={seatInARow}
+            rowNo={value}
+          />
+        );
+      })}
+    </div>
+  );
 }
